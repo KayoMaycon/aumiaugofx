6f927ef88c8f06f50147272a52774cc39f81de55
Kayo Maycon <102869922+KayoMaycon@users.noreply.github.com>
2023-07-19 15:26:59 -0300

    Criei index

---
+++ aumiaugofx/src/aumiaugofx/Index.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aumiaugofx;
+
+/**
+ *
+ * @author kayom
+ */
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
